0900823bcc6424875d5e2c56f144f2d6ecd8fc99
jongyoung3 <jongyong5645@gmail.com>
2024-06-28 16:17:20 +0900

    섹션 7 "의존관계 자동 주입"까지 완료

--- src/test/java/hello/core/beandefinition/BeanBDefinitionTest.java
+++ src/test/java/hello/core/beandefinition/BeanBDefinitionTest.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-public class BeanDefinitionTest {
+public class BeanBDefinitionTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 //     GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
---
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,34 @@
+package hello.core.scan;
+
+import hello.core.AutoAppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import hello.core.scan.filter.BeanB;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AutoAppConfigTest {
+
+    @Test
+    void basicScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+
+//        assertThrows(
+//                NoSuchBeanDefinitionException.class,
+//                () -> ac.getBean("beanB", BeanB.class));
+
+        OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
+    }
+}
